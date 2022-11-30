c0f6086bd084eff98ca44c322ee1596382875cb7
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2022-12-01 00:55:29 +0900

    Create data.lua

---
+++ PHI-EQ/data.lua
@@ -0,0 +1,4 @@
+require("prototypes.item.items")
+require("prototypes.equipment.equipment")
+require("prototypes.recipe.recipe-equipment")
+require("prototypes.technology.technology-compound-power")
