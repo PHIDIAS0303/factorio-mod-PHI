d8042c3a8e532feaa6de4401a170031855ef0686
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2022-12-01 21:55:23 +0900

    Update prototypes.lua

--- PHI-EN/prototypes.lua
+++ PHI-EN/prototypes.lua
@@ -69,6 +69,164 @@ items = {"accumulator", "solar-panel"}
 for i=1, 2, 1 do
     for j=2, 8, 1 do
         EE(items[i], j)
-        ER(items[i], j)
+        -- ER(items[i], j)
     end
 end
+
+data:extend(
+{
+  {
+    type = "recipe",
+    name = "solar-panel-mk2",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel", 4}
+    },
+    result = "solar-panel-mk2"
+  },
+  {
+    type = "recipe",
+    name = "solar-panel-mk3",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel-mk2", 4}
+    },
+    result = "solar-panel-mk3"
+  },
+  {
+    type = "recipe",
+    name = "solar-panel-mk4",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel-mk3", 4}
+    },
+    result = "solar-panel-mk4"
+  },
+  {
+    type = "recipe",
+    name = "solar-panel-mk5",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel-mk4", 4}
+    },
+    result = "solar-panel-mk5"
+  },
+  {
+    type = "recipe",
+    name = "solar-panel-mk6",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel-mk5", 4}
+    },
+    result = "solar-panel-mk6"
+  },
+  {
+    type = "recipe",
+    name = "solar-panel-mk7",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel-mk6", 4}
+    },
+    result = "solar-panel-mk7"
+  },
+  {
+    type = "recipe",
+    name = "solar-panel-mk8",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"solar-panel-mk7", 4}
+    },
+    result = "solar-panel-mk8"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk2",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator", 4}
+    },
+    result = "accumulator-mk2"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk3",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator-mk2", 4}
+    },
+    result = "accumulator-mk3"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk4",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator-mk3", 4}
+    },
+    result = "accumulator-mk4"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk5",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator-mk4", 4}
+    },
+    result = "accumulator-mk5"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk6",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator-mk5", 4}
+    },
+    result = "accumulator-mk6"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk7",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator-mk6", 4}
+    },
+    result = "accumulator-mk7"
+  },
+  {
+    type = "recipe",
+    name = "accumulator-mk8",
+    energy_required = 5,
+    enabled = "false",
+    ingredients =
+    {
+      {"accumulator-mk7", 4}
+    },
+    result = "accumulator-mk8"
+  }
+})
