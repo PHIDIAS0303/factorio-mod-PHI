8cff2bda2d283a5258b910be0b5957a88977358f
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-17 20:18:52 +0900

    Create migrations.lua

---
+++ PHI-MB/migrations/migrations.lua
@@ -0,0 +1,24 @@
+for index, force in pairs(game.forces) do
+    local technologies = force.technologies
+    local recipes = force.recipes
+    
+    if technologies['compound-energy-1'].researched then
+        recipes['assembling-machine-4'].enabled = true
+        recipes['assembling-machine-4'].reload()
+        recipes['electric-furnace-2'].enabled = true
+        recipes['electric-furnace-2'].reload()
+        recipes['chemical-plant-2'].enabled = true
+        recipes['chemical-plant-2'].reload()
+        recipes['oil-refinery-2'].enabled = true
+        recipes['oil-refinery-2'].reload()
+    elseif technologies['compound-energy-2'].researched then
+        recipes['assembling-machine-5'].enabled = true
+        recipes['assembling-machine-5'].reload()
+        recipes['electric-furnace-3'].enabled = true
+        recipes['electric-furnace-3'].reload()
+        recipes['chemical-plant-3'].enabled = true
+        recipes['chemical-plant-3'].reload()
+        recipes['oil-refinery-3'].enabled = true
+        recipes['oil-refinery-3'].reload()
+    end
+end
