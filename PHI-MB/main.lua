8d75410b5fa09b87cd1e7924aa6efa2c0d34df78
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-17 20:06:05 +0900

    Update main.lua

--- PHI-MB/main.lua
+++ PHI-MB/main.lua
@@ -17,6 +17,8 @@ local function EE(source, tier)
         local item = table.deepcopy(data.raw['furnace']['electric-furnace'])
     elseif source == 'chemical-plant' then
         local item = table.deepcopy(data.raw['assembling-machine']['chemical-plant'])
+    elseif source == 'oil-refinery' then
+        local item = table.deepcopy(data.raw['assembling-machine']['oil-refinery'])
     else
         local item = table.deepcopy(data.raw[source][source])
     end
