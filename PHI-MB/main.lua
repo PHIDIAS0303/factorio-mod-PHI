b7006760f71d0227925e5f3f637654ee28e2750a
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-17 20:33:51 +0900

    Update main.lua

--- PHI-MB/main.lua
+++ PHI-MB/main.lua
@@ -34,8 +34,8 @@ local function EE(source, tier)
     item.max_health = item.max_health * (2 ^ (tier - 1))
     item.crafting_speed = item.crafting_speed * (2 ^ (tier - 1))
     item.energy_usage = tonumber(string.match(item.energy_usage, '%d+')) * (2 ^ (tier - 1)) .. string.match(item.energy_usage, '%a')
-    item.picture.layers[1].filename = graphics_location .. source .. '-e.png'
-    item.picture.layers[1].hr_version.filename = graphics_location .. source ..'-eh.png'
+    item.animation.layers[1].filename = graphics_location .. source .. '-e.png'
+    item.animation.layers[1].hr_version.filename = graphics_location .. source ..'-eh.png'
     item.icon = graphics_location .. source .. '-i.png'
     item.icon_size = 64
     item.icon_mipmaps = 4
