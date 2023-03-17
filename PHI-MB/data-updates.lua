d77549fa2219b039f67773fafc430a9de78d0cb8
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-17 19:35:07 +0900

    Create data-updates.lua

---
+++ PHI-MB/data-updates.lua
@@ -0,0 +1,2 @@
+-- assembling-machine
+data.raw['assembling-machine']['assembling-machine-mk4'].fast_replaceable_group = data.raw['assembling-machine']['assembling-machine-3'].fast_replaceable_group
