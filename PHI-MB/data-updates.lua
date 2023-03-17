ee99294079e00a9766d2991dc08cea070ff4f2d3
PHIDIAS <10016767+PHIDIAS0303@users.noreply.github.com>
2023-03-17 19:53:45 +0900

    Update data-updates.lua

--- PHI-MB/data-updates.lua
+++ PHI-MB/data-updates.lua
@@ -1,2 +1,11 @@
 -- assembling-machine
-data.raw['assembling-machine']['assembling-machine-mk4'].fast_replaceable_group = data.raw['assembling-machine']['assembling-machine-3'].fast_replaceable_group
+data.raw['assembling-machine']['assembling-machine-4'].fast_replaceable_group = data.raw['assembling-machine']['assembling-machine-3'].fast_replaceable_group
+data.raw['assembling-machine']['assembling-machine-5'].fast_replaceable_group = data.raw['assembling-machine']['assembling-machine-4'].fast_replaceable_group
+
+-- electric-furnace
+data.raw['furnace']['electric-furnace-2'].fast_replaceable_group = data.raw['furnace']['electric-furnace'].fast_replaceable_group
+data.raw['furnace']['electric-furnace-3'].fast_replaceable_group = data.raw['furnace']['electric-furnace-2'].fast_replaceable_group
+
+-- chemical-plant
+data.raw['assembling-machine']['chemical-plant-2'].fast_replaceable_group = data.raw['assembling-machine']['chemical-plant'].fast_replaceable_group
+data.raw['assembling-machine']['chemical-plant-3'].fast_replaceable_group = data.raw['assembling-machine']['chemical-plant-2'].fast_replaceable_group
