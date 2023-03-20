90b69f08b4136283900fe33e8c7a9cf9fad3c172
PHIDIAS <phidias@aperx.org>
2023-03-20 23:41:46 +0900

    Update main.lua

--- PHI-MB/main.lua
+++ PHI-MB/main.lua
@@ -97,7 +97,7 @@ local function ER(source, tier)
     data:extend({{
         type = 'recipe',
         name = source .. '-' .. tier,
-        energy_required = 5,
+        energy_required = 2,
         enabled = false,
         ingredients = {{na, 2}},
         result = source .. '-' .. tier,
