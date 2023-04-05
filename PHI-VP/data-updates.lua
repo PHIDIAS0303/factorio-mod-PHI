d50d6b55756ecb9aad5fc20013284564957b1784
PHIDIAS <phidias@aperx.org>
2023-04-05 23:21:39 +0900

    Create data-updates.lua

---
+++ PHI-VP/data-updates.lua
@@ -0,0 +1,3 @@
+-- electric boiler
+data.raw['boiler']['boiler'].fast_replaceable_group = 'boiler'
+data.raw['boiler']['electric-boiler'].fast_replaceable_group = data.raw['boiler']['electric-boiler'].fast_replaceable_group
