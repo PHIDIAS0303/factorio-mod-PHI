77f517535393d89a98c6680b3996de6fcfdc2ccd
PHIDIAS <phidias@aperx.org>
2023-05-08 23:59:35 +0900

    Update

--- PHI-WE/data-updates.lua
+++ PHI-WE/data-updates.lua
@@ -12,3 +12,8 @@ data.raw['ammo-turret']['gun-turret-3'].fast_replaceable_group = data.raw['ammo-
 data.raw['fluid-turret']['flamethrower-turret'].fast_replaceable_group = 'flamethrower-turret'
 data.raw['fluid-turret']['flamethrower-turret-2'].fast_replaceable_group = data.raw['fluid-turret']['flamethrower-turret'].fast_replaceable_group
 data.raw['fluid-turret']['flamethrower-turret-3'].fast_replaceable_group = data.raw['fluid-turret']['flamethrower-turret-2'].fast_replaceable_group
+
+-- radar
+data.raw['radar']['radar'].fast_replaceable_group = 'radar'
+data.raw['radar']['radar-2'].fast_replaceable_group = data.raw['radar']['radar'].fast_replaceable_group
+data.raw['radar']['radar-3'].fast_replaceable_group = data.raw['radar']['radar-2'].fast_replaceable_group
