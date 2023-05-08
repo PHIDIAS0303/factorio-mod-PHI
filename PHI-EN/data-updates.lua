976b2f03df634dcf891ae340c40c3bf49c26bf57
PHIDIAS <phidias@aperx.org>
2023-05-09 01:45:44 +0900

    Update

--- PHI-EN/data-updates.lua
+++ PHI-EN/data-updates.lua
@@ -17,3 +17,17 @@ data.raw['solar-panel']['solar-panel-5'].fast_replaceable_group = data.raw['sola
 data.raw['solar-panel']['solar-panel-6'].fast_replaceable_group = data.raw['solar-panel']['solar-panel-5'].fast_replaceable_group
 data.raw['solar-panel']['solar-panel-7'].fast_replaceable_group = data.raw['solar-panel']['solar-panel-6'].fast_replaceable_group
 data.raw['solar-panel']['solar-panel-8'].fast_replaceable_group = data.raw['solar-panel']['solar-panel-7'].fast_replaceable_group
+
+-- boiler
+data.raw['boiler']['boiler'].fast_replaceable_group = 'boiler'
+data.raw['boiler']['boiler-2'].fast_replaceable_group = data.raw['boiler']['boiler'].fast_replaceable_group
+data.raw['boiler']['boiler-3'].fast_replaceable_group = data.raw['boiler']['boiler-2'].fast_replaceable_group
+data.raw['boiler']['boiler-4'].fast_replaceable_group = data.raw['boiler']['boiler-3'].fast_replaceable_group
+data.raw['boiler']['boiler-5'].fast_replaceable_group = data.raw['boiler']['boiler-4'].fast_replaceable_group
+
+-- steam-engine
+data.raw['generator']['steam-engine'].fast_replaceable_group = 'steam-engine'
+data.raw['generator']['steam-engine-2'].fast_replaceable_group = data.raw['generator']['steam-engine'].fast_replaceable_group
+data.raw['generator']['steam-engine-3'].fast_replaceable_group = data.raw['generator']['steam-engine-2'].fast_replaceable_group
+data.raw['generator']['steam-engine-4'].fast_replaceable_group = data.raw['generator']['steam-engine-3'].fast_replaceable_group
+data.raw['generator']['steam-engine-5'].fast_replaceable_group = data.raw['generator']['steam-engine-4'].fast_replaceable_group
