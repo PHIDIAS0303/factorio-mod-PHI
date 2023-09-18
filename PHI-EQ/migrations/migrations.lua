ccfcae18d865aca76aea20c764e792268c5d0135
PHIDIAS <phidias@aperx.org>
2023-09-18 20:53:55 +0900

    .

--- PHI-EQ/migrations/migrations.lua
+++ PHI-EQ/migrations/migrations.lua
@@ -4,13 +4,15 @@ for _, force in pairs(game.forces) do
     local technologies = force.technologies
     local recipes = force.recipes
 
-    for _, v in pairs(items) do
-        if v.enabled then
-            if technologies[v.tech] ~= nil then
-                if technologies[v.tech].researched then
-                    for j=v.min, v.max, 1 do
-                        recipes[v.name .. '-' .. j].enabled = true
-                        recipes[v.name .. '-' .. j].reload()
+    for k, v in pairs(items) do
+        if k ~= 'setting' then
+            if v.enabled then
+                if technologies[v.tech] ~= nil then
+                    if technologies[v.tech].researched then
+                        for j=v.min, v.max, 1 do
+                            recipes[v.name .. '-' .. j].enabled = true
+                            recipes[v.name .. '-' .. j].reload()
+                        end
                     end
                 end
             end
