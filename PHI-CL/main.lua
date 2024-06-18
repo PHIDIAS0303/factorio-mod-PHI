e73a03b499268a932087a973cb4d61b1a43c08bf
PHIDIAS <phidias@aperx.org>
2024-06-18 18:25:11 +0900

    .

--- PHI-CL/main.lua
+++ PHI-CL/main.lua
@@ -1,5 +1,5 @@
 local alpha_order = {'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'}
--- local graphics_location = '__PHI-EN__/graphics/'
+-- local graphics_location = '__PHI-CL__/graphics/'
 local items = require 'config'
 local main = {}
 
