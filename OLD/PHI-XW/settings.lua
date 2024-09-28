8a258826ed02bcbb7ef80da971a721c087e8d4f0
PHIDIAS <phidias@aperx.org>
2024-09-28 13:46:03 +0900

    .

--- OLD/PHI-XW/settings.lua
+++
@@ -1,8 +0,0 @@
-data:extend({{
-    type = 'int-setting',
-    name = 'PHI-XW-WATER',
-    setting_type = 'startup',
-    default_value = 5,
-    allowed_values = {1, 2, 3, 4, 5},
-    order = 'A1'
-  }})
