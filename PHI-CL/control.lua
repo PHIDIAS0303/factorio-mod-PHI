a7a5d3d43459cf65baba7f39e58f79db27307311
PHIDIAS <phidias@aperx.org>
2025-03-26 19:58:13 +0900

    .

--- PHI-CL/control.lua
+++ PHI-CL/control.lua
@@ -42,7 +42,7 @@ if settings.startup['PHI-CT'].value then
         local force = game.players[e.player_index].force
 
         for _, v in pairs(prototypes.fluid) do
-            force.recipes['pump-' .. v.name].enabled = enable
+            force.recipes['pump-' .. v.name].enabled = (enable and prototypes.fluid[v.name].hidden)
         end
 
         force.recipes['super-radar'].enabled = enable
