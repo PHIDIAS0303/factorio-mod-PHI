deb5f3a1bdf93ca81fa5b44854c26b7005cb7076
PHIDIAS <phidias@aperx.org>
2025-04-03 00:14:44 +0900

    .

--- PHI-CL/data-final-fixes.lua
+++ PHI-CL/data-final-fixes.lua
@@ -2,9 +2,16 @@ local items = require 'config'
 local main = require 'main'
 
 if mods['space-age'] and settings.startup['PHI-VP'].value and settings.startup['PHI-VP-MAIN'].value then
+    local vp = settings.startup['PHI-VP'].value and settings.startup['PHI-VP-MAIN'].value
+    local sa = settings.startup['PHI-SA'].value and settings.startup['PHI-SA-GENERIC'].value
+
     for k, v in pairs(data.raw.recipe) do
-        if v.category == 'recycling' then
+        if vp and v.category == 'recycling' then
             data.raw.recipe[k] = nil
+        end
+
+        if (vp or sa) and v.maximum_productivity then
+            v.maximum_productivity = nil
         end
     end
 end
