4d79f1862192205a7bf8695661307711e96e4b0e
PHIDIAS <phidias@aperx.org>
2025-04-04 18:56:00 +0900

    .

--- PHI-CL/data.lua
+++ PHI-CL/data.lua
@@ -440,8 +440,8 @@ if mods['space-age'] and ((settings.startup['PHI-SA'].value and settings.startup
     end
 
     data.raw['module']['efficiency-module'].effect.consumption = -0.3
-    data.raw['module']['efficiency-module-2'].effect.consumption = -0.5
-    data.raw['module']['efficiency-module-3'].effect.consumption = -0.7
+    data.raw['module']['efficiency-module-2'].effect.consumption = -0.6
+    data.raw['module']['efficiency-module-3'].effect.consumption = -0.9
 
     for _, v in pairs({data.raw['module']['efficiency-module'], data.raw['module']['efficiency-module-2'], data.raw['module']['efficiency-module-3']}) do
         v.effect.consumption = v.effect.consumption * settings.startup['PHI-MI-EFFCY'].value / 100
