84c0cef5a89087a08de1854ea21e4fb20ddbf77c
PHIDIAS <phidias@aperx.org>
2025-04-07 21:02:48 +0900

    .

--- PHI-CL/control.lua
+++ PHI-CL/control.lua
@@ -118,6 +118,31 @@ if settings.startup['PHI-CT'].value then
         end
     end)
 
+    script.on_event(defines.events.on_gui_click, function(e)
+        local player = game.players[e.player_index]
+        local gui = player.gui.relative.inserter_config
+    end)
+
+    script.on_event(defines.events.on_player_rotated_entity, function(e)
+        if e.entity and (e.entity.type == 'inserter' or (e.entity.type == 'entity-ghost' and e.entity.ghost_type == 'inserter')) then
+            for _, player in pairs(game.players) do
+                if player.opened == e.entity then
+                    gui_update(player, player.opened)
+                end
+            end
+        end
+    end)
+
+    script.on_event(defines.events.on_entity_settings_pasted, function(e)
+        if e.destination.type == 'inserter' or (e.destination.type == 'entity-ghost' and e.destination.ghost_type == 'inserter') then
+            for _, player in pairs(game.players) do
+                if player.opened == e.destination then
+                    gui_update(player, player.opened)
+                end
+            end
+        end
+    end)
+
     --[[
     local gui = {}
     local inserter_utils = {}
