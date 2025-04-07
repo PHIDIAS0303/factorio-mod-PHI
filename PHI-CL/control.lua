5110391d66d445dbc6b97753e3531e3502891524
PHIDIAS <phidias@aperx.org>
2025-04-07 23:29:35 +0900

    .

--- PHI-CL/control.lua
+++ PHI-CL/control.lua
@@ -64,7 +64,21 @@ if settings.startup['PHI-CT'].value then
         hidden_recipe_enable(e)
     end)
 
-    function gui_create(player)
+    script.on_init(function(_)
+        for _, surface in pairs(game.surfaces) do
+            for _, e in pairs(surface.find_entities_filtered{name='trash-chest'}) do
+                e.infinity_container_filters = {}
+                e.remove_unfiltered_items = true
+            end
+
+            for _, e in pairs(surface.find_entities_filtered{name='trash-pipe'}) do
+                e.set_infinity_pipe_filter(nil)
+            end
+        end
+    end)
+
+    --[[
+        function gui_create(player)
         if player.gui.relative.inserter_config then
             player.gui.relative.inserter_config.destroy()
         end
@@ -207,7 +221,6 @@ if settings.startup['PHI-CT'].value then
         end
     end)
 
-    --[[
     local gui = {}
     local inserter_utils = {}
     math2d.direction = {vectors = {{x = 0, y = -1}, {x = 1, y = -1}, {x = 1, y = 0}, {x = 1, y = 1}, {x = 0, y = 1}, {x = -1, y = 1}, {x = -1, y = 0}, {x = -1, y = -1}}}
