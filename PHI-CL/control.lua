90cd59f7f83498f96b183b7f7e77f5b1e399d3a6
PHIDIAS <phidias@aperx.org>
2025-04-11 15:49:53 +0900

    .

--- PHI-CL/control.lua
+++ PHI-CL/control.lua
@@ -88,8 +88,8 @@ if settings.startup['PHI-CT'].value then
 
         local frame = player.gui.relative.add({type = 'frame', name = 'inserter_config', anchor = {gui = defines.relative_gui_type.inserter_gui, position = defines.relative_gui_position.right}})
         frame.add({type = 'label', name = 'label_info', caption = 'Inserter direction', style = 'heading_2_label'})
-        frame.add({type = 'drop-down', name = 'i_direction', items = {'[virtual-signal/down-arrow]', '[virtual-signal/left-arrow]', '[virtual-signal/up-arrow]', '[virtual-signal/right-arrow]'}, selected_index = 1})
-        frame.add({type = 'drop-down', name = 'i_sub_direction', items = {'[virtual-signal/signal-0]', '[virtual-signal/signal-1]', '[virtual-signal/signal-2]', '[virtual-signal/signal-3]'}, selected_index = 1})
+        frame.add({type = 'drop-down', name = 'i_direction', items = {'[virtual-signal=down-arrow]', '[virtual-signal=left-arrow]', '[virtual-signal=up-arrow]', '[virtual-signal=right-arrow]'}, selected_index = 1})
+        frame.add({type = 'drop-down', name = 'i_sub_direction', items = {'[virtual-signal=signal-0]', '[virtual-signal=signal-1]', '[virtual-signal=signal-2]', '[virtual-signal=signal-3]'}, selected_index = 1})
     end
 
     function gui_update(player, inserter)
