2c96c36d2e4bdbd5d21ff9615b85d208b2321920
PHIDIAS <phidias@aperx.org>
2025-06-03 17:52:22 +0900

    .

--- PHI-CL/data.lua
+++ PHI-CL/data.lua
@@ -839,14 +839,7 @@ if settings.startup['PHI-VP'].value then
                 energy_required = 5,
                 enabled = false,
                 category = 'crafting',
-                ingredients = {
-                    {type = 'item', name = 'low-density-structure', amount = 100},
-                    {type = 'item', name = 'solar-panel', amount = 100},
-                    {type = 'item', name = 'accumulator', amount = 100},
-                    {type = 'item', name = 'radar', amount = 5},
-                    {type = 'item', name = 'processing-unit', amount = 100},
-                    {type = 'item', name = 'rocket-fuel', amount = 50}
-                },
+                ingredients = {{type = 'item', name = 'low-density-structure', amount = 100}, {type = 'item', name = 'solar-panel', amount = 100}, {type = 'item', name = 'accumulator', amount = 100}, {type = 'item', name = 'radar', amount = 5}, {type = 'item', name = 'processing-unit', amount = 100}, {type = 'item', name = 'rocket-fuel', amount = 50}},
                 results = {{type = 'item', name = 'satellite', amount = 1}},
                 requester_paste_multiplier = 1
             }
