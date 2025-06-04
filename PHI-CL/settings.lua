d8928009150249ef784f2e28dc548982884bbdf5
PHIDIAS <phidias@aperx.org>
2025-06-05 02:03:45 +0900

    .

--- PHI-CL/settings.lua
+++ PHI-CL/settings.lua
@@ -211,3 +211,11 @@ data:extend({
   order = 'E02'
 }
 })
+
+if mods['space-age'] then
+    data.raw['bool-setting']['PHI-MB-EQUIPMENT-ARMOR'].localised_description = {'', '[item=power-armor-mk2] 3, [item=mech-armor] 2'}
+    data.raw['bool-setting']['PHI-SA-SPOIL'].localised_description = {'', '[item=spoilage]'}
+    data.raw['bool-setting']['PHI-SA-SPOIL-FREEZE'].localised_description = {'', '[item=ice]'}
+    data.raw['bool-setting']['PHI-SA-ENABLE-QUALITY'].localised_description = {'', '[virtual-signal=signal-any-quality]'}
+    data.raw['int-setting']['PHI-SA-QUALITY'].localised_description = {'', '[item=quality-module-3] x 0.1'}
+end
