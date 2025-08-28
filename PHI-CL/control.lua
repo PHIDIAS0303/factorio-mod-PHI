dd10a51f552e779efecf3a9ffe07818101bc4e09
PHIDIAS <phidias@aperx.org>
2025-08-28 18:44:11 +0900

    .

--- PHI-CL/control.lua
+++ PHI-CL/control.lua
@@ -145,11 +145,11 @@ local function entity_build(event)
             return
         end
 
-        local e = table.deepcopy(event.entity)
+        local e = event.entity
         event.entity.destroy()
 
         local p = e.surface.create_entity{name = 'proxy-cargo-landing-pad', position = {e.position.x, e.position.y}, force = 'neutral', quality = e.quality.name}
-        p.proxy_target_entity = e
+        p.proxy_target_entity = ec[1]
         p.proxy_target_inventory = defines.inventory.cargo_landing_pad_main
 
         return
