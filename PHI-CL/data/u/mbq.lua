52ff2e34ed02fd136158665767b478706df548de
PHIDIAS <phidias@aperx.org>
2026-03-19 21:52:57 +0900

    .

--- PHI-CL/data/u/mbq.lua
+++ PHI-CL/data/u/mbq.lua
@@ -1,23 +1,5 @@
-local main = require 'main'
-local items = require 'mbq-c'
-
--- MBQ A 48 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
-for _, v in pairs(items) do
-    v.mod = v.mod or 'base'
-    v.min = v.min or 2
-
-    if v.enabled and (v.mod and mods[v.mod]) and (v.max >= v.min) then
-        v.category = 'equipment'
-        v.ref_name = v.ref_name or v.name .. '-equipment'
-
-        for j=v.min, v.max, 1 do
-            main.EEQ(v, j)
-            main.EI(v, j)
-            main.ER(v, j)
-            main.ET(v, j)
-        end
-    end
-end
+-- local main = require 'main'
+-- local items = require 'mbq-c'
 
 -- MBQ C 11 BASE ARMOR_EQUIPMENT
 if settings.startup['PHI-MB-EQUIPMENT-SIZE'].value then
