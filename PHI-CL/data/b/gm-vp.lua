5b27dcae74bebc2a8fea5501f778c174170be23b
PHIDIAS <phidias@aperx.org>
2026-05-07 22:48:59 +0900

    .

--- PHI-CL/data/b/gm-vp.lua
+++ PHI-CL/data/b/gm-vp.lua
@@ -239,6 +239,12 @@ for _, v in pairs({'space-science-pack', 'electromagnetic-plant', 'foundry', 'cr
     end
 end
 
+-- GM-VP C 1 BASE ENTITY
+-- GM-VP C 1 SPACE_AGE ENTITY
+for _, v in pairs(data.raw.lab) do
+    v.inputs = {'automation-science-pack', 'logistic-science-pack', 'military-science-pack', 'chemical-science-pack', 'production-science-pack', 'utility-science-pack', 'space-science-pack'}
+end
+
 -- GM-VP H 4 SPACE_AGE ITEM
 for _, v in pairs({'carbonic', 'metallic', 'promethium', 'oxide'}) do
     local l = v .. '-asteroid-chunk'
@@ -340,9 +346,7 @@ data.raw['lab']['biolab'].energy_source.emissions_per_minute = nil
 data.raw['electric-turret']['tesla-turret'].energy_source.drain = '250kW'
 data.raw['chain-active-trigger']['chain-tesla-turret-chain'].fork_chance = 0.3
 
-for _, v in pairs(data.raw.lab) do
-    v.inputs = {'automation-science-pack', 'logistic-science-pack', 'military-science-pack', 'chemical-science-pack', 'production-science-pack', 'utility-science-pack', 'space-science-pack'}
-end
+
 
 for _, v in pairs({'vulcanus', 'gleba', 'fulgora', 'aquilo'}) do
     data.raw.planet[v].map_gen_settings = nil
@@ -355,7 +359,7 @@ for _, v in pairs({'platform_science', 'platform_moving', 'platform_messy_nuclea
     data.raw['utility-constants']['default']['main_menu_simulations'][v] = nil
 end
 
-data.raw['dont-build-entity-achievement']['logistic-network-embargo'].research_with = nil
+data.raw['dont-build-entity-achievement']['logistic-network-embargo'] = nil
 data.raw['create-platform-achievement']['reach-for-the-stars'] = nil
 data.raw['complete-objective-achievement']['second-star-to-the-right-and-straight-on-till-morning'] = nil
 data.raw['dont-research-before-researching-achievement']['rush-to-space'] = nil
@@ -388,14 +392,6 @@ for _, v in pairs({'vulcanus_coal', 'tungsten_ore', 'calcite', 'sulfuric_acid_ge
     end
 end
 
-for _, v in pairs({'lichen-decal', 'shroom-decal', 'pink-lichen-decal', 'red-lichen-decal', 'yellow-lettuce-lichen-1x1', 'yellow-lettuce-lichen-3x3', 'yellow-lettuce-lichen-6x6', 'yellow-lettuce-lichen-cups-1x1', 'yellow-lettuce-lichen-cups-3x3', 'yellow-lettuce-lichen-cups-6x6', 'honeycomb-fungus', 'honeycomb-fungus-1x1', 'honeycomb-fungus-decayed', 'green-lettuce-lichen-1x1', 'green-lettuce-lichen-3x3', 'green-lettuce-lichen-6x6', 'green-lettuce-lichen-water-1x1', 'green-lettuce-lichen-water-3x3', 'green-lettuce-lichen-water-6x6', 'split-gill-1x1', 'split-gill-2x2', 'split-gill-dying-1x1', 'split-gill-dying-2x2', 'split-gill-red-1x1', 'split-gill-red-2x2', 'fuchsia-pita', 'wispy-lichen', 'coral-land', 'coral-water', 'black-sceptre', 'pink-phalanges', 'green-cup', 'mycelium', 'veins', 'veins-small', 'brambles', 'blood-grape', 'blood-grape-vibrant', 'brown-cup', 'polycephalum-slime', 'polycephalum-balloon', 'grey-cracked-mud-decal', 'yellow-coral', 'solo-barnacle', 'curly-roots-orange', 'curly-roots-grey', 'pale-lettuce-lichen-cups-1x1', 'pale-lettuce-lichen-cups-3x3', 'pale-lettuce-lichen-cups-6x6', 'pale-lettuce-lichen-1x1', 'pale-lettuce-lichen-3x3', 'pale-lettuce-lichen-6x6', 'pale-lettuce-lichen-water-1x1', 'pale-lettuce-lichen-water-3x3', 'pale-lettuce-lichen-water-6x6', 'matches-small', 'nerve-roots-dense', 'nerve-roots-sparse', 'red-nerve-roots-veins-dense', 'red-nerve-roots-veins-sparse', 'purple-nerve-roots-veins-dense', 'purple-nerve-roots-veins-sparse', 'cream-nerve-roots-veins-dense', 'cream-nerve-roots-veins-sparse', 'white-carpet-grass', 'barnacles-decal', 'coral-stunted-grey', 'coral-stunted', 'gleba-spawner-slime', 'knobbly-roots', 'knobbly-roots-orange', 'fulgoran-ruin-tiny', 'fulgoran-gravewort', 'urchin-cactus', 'medium-fulgora-rock', 'small-fulgora-rock', 'tiny-fulgora-rock', 'aqulio-ice-decal-blue', 'aqulio-snowy-decal', 'floating-iceberg-large', 'floating-iceberg-small', 'lithium-iceberg-medium', 'lithium-iceberg-small', 'lithium-iceberg-tiny', 'snow-drift-decal', 'v-green-hairy-grass', 'v-brown-hairy-grass', 'v-brown-carpet-grass', 'v-red-pita', 'vulcanus-rock-decal-large', 'vulcanus-crack-decal', 'vulcanus-crack-decal-large', 'vulcanus-crack-decal-huge-warm', 'vulcanus-crack-decal-warm', 'calcite-stain', 'calcite-stain-small', 'sulfur-stain', 'sulfur-stain-small', 'sulfuric-acid-puddle', 'sulfuric-acid-puddle-small', 'crater-small', 'crater-large', 'pumice-relief-decal', 'vulcanus-sand-decal', 'vulcanus-dune-decal', 'waves-decal', 'medium-volcanic-rock', 'small-volcanic-rock', 'tiny-volcanic-rock', 'tiny-rock-cluster', 'small-sulfur-rock', 'tiny-sulfur-rock', 'sulfur-rock-cluster', 'vulcanus-lava-fire'}) do
-    data.raw['optimized-decorative'][v].autoplace = nil
-end
-
-data.raw['lightning-attractor']['fulgoran-ruin-attractor'].autoplace = nil
-
-
-
 for k, v in pairs(items['item']) do
     if data.raw.item[k] then
         data.raw.item[k].hidden = v
@@ -412,7 +408,6 @@ end
 
 for _, v in pairs(data.raw['tile']) do
     if v.subgroup and (v.subgroup == 'vulcanus-tiles' or v.subgroup == 'gleba-tiles' or v.subgroup == 'gleba-water-tiles' or v.subgroup == 'fulgora-tiles' or v.subgroup == 'aquilo-tiles') then
-        v.autoplace = nil
         v.hidden = true
         v.hidden_in_factoriopedia = true
     end
