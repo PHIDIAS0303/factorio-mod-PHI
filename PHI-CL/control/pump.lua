e69f825f32fe7ab27cb8093f69758bfcdcbf49d6
PHIDIAS <phidias@aperx.org>
2026-05-26 03:09:17 +0900

    .

--- PHI-CL/control/pump.lua
+++ PHI-CL/control/pump.lua
@@ -4,7 +4,7 @@ function main.recipe_hidden(event)
     local hidden = (event.name == defines.events.on_player_cheat_mode_enabled and true) or false
 
     for _, v in pairs(prototypes.fluid) do
-        if v.subgroup == 'fluid' then
+        if v.subgroup == 'super-pump-fluid' then
             prototypes.recipe['super-pump-' .. v.name].hidden = hidden
         end
     end
