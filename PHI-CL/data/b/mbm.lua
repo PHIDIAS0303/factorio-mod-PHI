9dadf9ace277c71b1aadaeb664370fc9e82137b4
PHIDIAS <phidias@aperx.org>
2026-06-05 18:16:37 +0900

    .

--- PHI-CL/data/b/mbm.lua
+++ PHI-CL/data/b/mbm.lua
@@ -26,9 +26,9 @@ end
 if data.raw['assembling-machine'] and data.raw['assembling-machine']['assembling-machine'] and settings.startup['PHI-MB-MACHINE'].value > 1 then
     for i = 4, settings.startup['PHI-MB-MACHINE'].value + 2 do
         if data.raw['assembling-machine']['assembling-machine-' .. i] then
-            data.raw['assembling-machine']['assembling-machine-' .. i].localised_name = {'phi-cl.combine', {'?', {'entity-name.assembling-machine'}, {'name.assembling-machine'}}, tostring(i)}
-            data.raw['item']['assembling-machine-' .. i].localised_name = {'phi-cl.combine', {'?', {'entity-name.assembling-machine'}, {'name.assembling-machine'}}, tostring(i)}
-            data.raw['recipe']['assembling-machine-' .. i].localised_name = {'phi-cl.combine', {'?', {'entity-name.assembling-machine'}, {'name.assembling-machine'}}, tostring(i)}
+            data.raw['assembling-machine']['assembling-machine-' .. i].localised_name = {'phi-cl.combine', {'entity-name.assembling-machine-1'}, tostring(i)}
+            data.raw['item']['assembling-machine-' .. i].localised_name = {'phi-cl.combine', {'entity-name.assembling-machine-1'}, tostring(i)}
+            data.raw['recipe']['assembling-machine-' .. i].localised_name = {'phi-cl.combine', {'entity-name.assembling-machine-1'}, tostring(i)}
         end
     end
 end
