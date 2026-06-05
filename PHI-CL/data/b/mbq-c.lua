fe1c543ab8ca24b026f173cacaa5ac84b4d53657
PHIDIAS <phidias@aperx.org>
2026-06-05 17:52:23 +0900

    .

--- PHI-CL/data/b/mbq-c.lua
+++ PHI-CL/data/b/mbq-c.lua
@@ -6,14 +6,14 @@ return {
         ref_name = (mods['space-age'] and 'battery-mk3-equipment') or 'battery-mk2-equipment',
         tech = (mods['space-age'] and 'battery-mk3-equipment') or 'battery-mk2-equipment',
         min = (mods['space-age'] and 4) or 3,
-        max = (tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1) + ((mods['space-age'] and 2) or 1)
+        max = settings.startup['PHI-MB-EQUIPMENT'].value + ((mods['space-age'] and 2) or 1)
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['discharge-defense-equipment'] = {
         type = 'active-defense-equipment',
         name = 'discharge-defense',
         tech = 'discharge-defense-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['energy-shield-equipment'] = {
@@ -22,14 +22,14 @@ return {
         ref_name = 'energy-shield-mk2-equipment',
         tech = 'energy-shield-mk2-equipment',
         min = 3,
-        max = (tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1) + 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value + 1
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['exoskeleton-equipment'] = {
         type = 'movement-bonus-equipment',
         name = 'exoskeleton',
         tech = 'exoskeleton-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value or 1
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['fission-reactor-equipment'] = {
@@ -37,21 +37,21 @@ return {
         name = 'fission-reactor',
         ref_name = 'fission-reactor-equipment',
         tech = 'fission-reactor-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value or 1
     },
     -- MBQ A 1 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['night-vision-equipment'] = {
         type = 'night-vision-equipment',
         name = 'night-vision',
         tech = 'night-vision-equipment',
-        max = math.min(tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1, 2)
+        max = math.min(settings.startup['PHI-MB-EQUIPMENT'].value, 2)
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['personal-laser-defense-equipment'] = {
         type = 'active-defense-equipment',
         name = 'personal-laser-defense',
         tech = 'personal-laser-defense-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['personal-roboport-equipment'] = {
@@ -60,14 +60,14 @@ return {
         ref_name = 'personal-roboport-mk2-equipment',
         tech = 'personal-roboport-mk2-equipment',
         min = 3,
-        max = (tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1) + 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value + 1
     },
     -- MBQ A 2 BASE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['solar-panel-equipment'] = {
         type = 'solar-panel-equipment',
         name = 'solar-panel',
         tech = 'solar-panel-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1,
+        max = settings.startup['PHI-MB-EQUIPMENT'].value,
         base = 30,
         graphics_name = 'solar-panel-equipment'
     },
@@ -77,7 +77,7 @@ return {
         type = 'generator-equipment',
         name = 'fusion-reactor',
         tech = 'fusion-reactor-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value
     },
     -- MBQ A 2 SPACE_AGE ARMOR_EQUIPMENT,RECIPE,RESEARCH_EFFECT
     ['toolbelt-equipment'] = {
@@ -85,6 +85,6 @@ return {
         type = 'inventory-bonus-equipment',
         name = 'toolbelt',
         tech = 'toolbelt-equipment',
-        max = tonumber(settings.startup['PHI-MB-EQUIPMENT'].value) or 1
+        max = settings.startup['PHI-MB-EQUIPMENT'].value
     }
 }
