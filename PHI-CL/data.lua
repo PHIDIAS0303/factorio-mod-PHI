3df0aa5e6b87b87c501d4d502c5457401a739660
PHIDIAS <phidias@aperx.org>
2026-06-05 15:42:42 +0900

    .

--- PHI-CL/data.lua
+++ PHI-CL/data.lua
@@ -1,3 +1,16 @@
+if settings.startup['PHI-MB'].value then
+    if settings.startup['PHI-MB-ENERGY'].value and settings.startup['PHI-MB-ENERGY'].value > 1 then
+        require('data.b.mbe')
+    end
+
+    if settings.startup['PHI-MB-MACHINE'].value and settings.startup['PHI-MB-MACHINE'].value > 1 then
+        require('data.b.mbm')
+    end
+
+    if settings.startup['PHI-MB-EQUIPMENT'].value and settings.startup['PHI-MB-EQUIPMENT'].value > 1 then
+        require('data.b.mbq')
+    end
+end
 
 if settings.startup['PHI-CT'].value then
     require('data.b.ct')
@@ -25,19 +38,5 @@ if settings.startup['PHI-GM'].value then
 
     elseif settings.startup['PHI-GM'].value == 'VP' then
         require('data.b.gm-vp')
-    end
-end
-
-if settings.startup['PHI-MB'].value then
-    if settings.startup['PHI-MB-ENERGY'].value and settings.startup['PHI-MB-ENERGY'].value > 1 then
-        require('data.b.mbe')
-    end
-
-    if settings.startup['PHI-MB-MACHINE'].value and settings.startup['PHI-MB-MACHINE'].value > 1 then
-        require('data.b.mbm')
-    end
-
-    if settings.startup['PHI-MB-EQUIPMENT'].value and settings.startup['PHI-MB-EQUIPMENT'].value > 1 then
-        require('data.b.mbq')
     end
 end
